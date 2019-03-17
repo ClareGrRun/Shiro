3beb9162a42959071ea6907a1e0300fd169a38b3
Administrator <1145265355@qq.com>
2019-03-17 22:56:03 +0800

    修改

--- src/main/java/com/shiro/mapper/ShiroMapper.java
+++ src/main/java/com/shiro/mapper/ShiroMapper.java
@@ -1,8 +1,11 @@
 package com.shiro.mapper;
 
+import com.shiro.pojo.Menu;
 import com.shiro.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 
 @Mapper
@@ -10,4 +13,8 @@ public interface ShiroMapper {
    User findUserByName(@Param("username") String username);
 
    String findRoleById(Integer id);
+
+   List<Menu> patterns();
+
+   List<String> roles(Integer id);
 }
